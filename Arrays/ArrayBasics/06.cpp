6e4ecb73de6445b8def06880d8e728abea6c9363
Dhiraj Maurya <mauryadheeraj226@gmail.com>
2026-07-16 13:07:09 +0530

    Add array basics solutions

---
+++ Arrays/ArrayBasics/06.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> getAlternates(vector<int> &arr)
+    {
+        vector<int> ans;
+        // code here
+        for (int i = 0; i < arr.size(); i += 2)
+        {
+            ans.push_back(arr[i]);
+        }
+        return ans;
+    }
+};
