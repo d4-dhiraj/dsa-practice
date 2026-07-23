8ea2e516f901f24551dc2558eb1743f6e3724274
Dhiraj Maurya <mauryadheeraj226@gmail.com>
2026-07-23 07:40:34 +0530

    Add array advance  question

---
+++ Arrays/AdvanceProblems/04.cpp
@@ -0,0 +1,17 @@
+class Solution
+{
+public:
+    int maxProfit(vector<int> &prices)
+    {
+        // BruteForce Approach
+        //  int profit = 0;
+        //  for(int i = 0; i < prices.size(); i++){
+        //      for(int j = i + 1; j < prices.size(); j++){
+        //          profit = max(profit, prices[j]- prices[i]);
+        //      }
+        //  }
+        //  return profit;
+
+        // Optemized Approach
+        //IO have to solve in O(n)
+};
