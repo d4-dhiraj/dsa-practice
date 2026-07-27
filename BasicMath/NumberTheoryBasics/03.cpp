f238f666bdadcf28ac94acc38539ed6465169b64
Dhiraj Maurya <mauryadheeraj226@gmail.com>
2026-07-27 07:48:32 +0530

    Add Basic Math question

---
+++ BasicMath/NumberTheoryBasics/03.cpp
@@ -0,0 +1,18 @@
+class Solution
+{
+public:
+    bool isPrime(int n)
+    {
+        // code here
+        if (n == 0 || n == 1)
+            return false;
+        for (int i = 2; i <= sqrt(n); i++)
+        {
+            if (n % i == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+};
