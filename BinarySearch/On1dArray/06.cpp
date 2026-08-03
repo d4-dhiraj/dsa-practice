562ad7d3476e0c612e386f0de92888fafd6fe137
Dhiraj Maurya <mauryadheeraj226@gmail.com>
2026-08-03 09:07:44 +0530

    Adding binary search basic question

---
+++ BinarySearch/On1dArray/06.cpp
@@ -0,0 +1,25 @@
+class Solution
+{
+public:
+    int findCeil(vector<int> &arr, int x)
+    {
+        // code here
+        int ans = -1;
+        int s = 0, e = arr.size() - 1;
+
+        while (s <= e)
+        {
+            int mid = s + (e - s) / 2;
+            if (arr[mid] >= x)
+            {
+                ans = mid;
+                e = mid - 1;
+            }
+            else
+            {
+                s = mid + 1;
+            }
+        }
+        return ans;
+    }
+};
