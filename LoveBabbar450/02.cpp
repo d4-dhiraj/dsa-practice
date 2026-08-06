5d4c4d732748297bd5e16ed7744f17ad8c926917
Dhiraj Maurya <mauryadheeraj226@gmail.com>
2026-08-06 13:43:17 +0530

    Adding Question

---
+++ LoveBabbar450/02.cpp
@@ -0,0 +1,26 @@
+class Solution
+{
+public:
+    vector<int> getMinMax(vector<int> &arr)
+    {
+        int min = INT_MAX;
+        int max = INT_MIN;
+        int n = arr.size();
+        for (int i = 0; i < n; i++)
+        {
+            if (arr[i] > max)
+            {
+                max = arr[i];
+            }
+        }
+        for (int i = 0; i < n; i++)
+        {
+            if (arr[i] < min)
+            {
+                min = arr[i];
+            }
+        }
+
+        return {min, max};
+    }
+};
